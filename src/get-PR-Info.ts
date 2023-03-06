c0db0be52b1f562f02ca9d7f01658fc22022afc0
JulissaDantes <julissadcj@gmail.com>
2023-03-06 10:30:42 -0400

    get PR info

---
+++ src/get-PR-Info.ts
@@ -0,0 +1,35 @@
+const { Octokit } = require("@octokit/rest");//const { Octokit, App } = require("octokit");
+const  fs  = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+async function getAllPRComments(){
+  try {
+      const response = await octokit.request(`GET /repos/${process.env.owner}/${process.env.repo}/pulls/6/comments`);
+      const result = Array.from(response.data);
+      console.log(result);
+      const data = result.map((r: any) => {return {'id': r.id, 'body': r.body}} );
+      await fs.promises.writeFile('results.json', JSON.stringify(data));
+      console.table(data);
+    
+    } catch (error) {
+      console.log(error)
+    }
+}
+getAllPRComments();
+
+async function getPRInfo(){
+  try {
+      const response = await octokit.request(`GET /repos/${process.env.owner}/${process.env.repo}/pulls/6`);
+      const result = response.data.body;
+      console.log(result);    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getPRInfo();
